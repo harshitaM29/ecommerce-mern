a10cce8e294e583555e71a0f63ce36b54bb8b287
harshitaM29 <harshita.mundhe29@gmail.com>
2024-03-01 11:27:27 +0530

    categories UI implementation

--- ecommerce-ui/src/pages/Home.js
+++ ecommerce-ui/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Announment from "../components/Announment";
 import Slider from "../components/Slider";
+import Categories from "../components/Categories";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Announment />
       <Navbar />
       <Slider />
+      <Categories />
     </div>
   );
 };
