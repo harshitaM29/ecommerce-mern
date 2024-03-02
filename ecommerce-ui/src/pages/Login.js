d8e097c50165587979f716a019bf2d42e7c113a1
harshitaM29 <harshita.mundhe29@gmail.com>
2024-03-02 11:18:36 +0530

    Register and login page component

--- ecommerce-ui/src/pages/Login.js
+++ ecommerce-ui/src/pages/Login.js
@@ -7,7 +7,7 @@ const Container = styled.div`
       rgba(255, 255, 255, 0.5),
       rgba(255, 255, 255, 0.5)
     ),
-    url("https://images.pexels.com/photos/6984661/pexels-photo-6984661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
+    url("https://images.pexels.com/photos/6984650/pexels-photo-6984650.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
       center;
   background-size: cover;
   display: flex;
@@ -15,7 +15,7 @@ const Container = styled.div`
   justify-content: center;
 `;
 const Wrapper = styled.div`
-  width: 40%;
+  width: 25%;
   padding: 20px;
   background-color: white;
 `;
@@ -25,46 +25,46 @@ const Title = styled.h1`
 `;
 const Form = styled.form`
   display: flex;
-  flex-wrap: wrap;
+  flex-direction: column;
 `;
 const Input = styled.input`
   flex: 1;
   min-width: 40%;
-  margin: 20px 10px 0px 0px;
+  margin: 10px 0px;
   padding: 8px;
 `;
-const Agreement = styled.span`
-  margin: 20px 0px;
-`;
+
 const Button = styled.button`
   width: 40%;
   border: none;
-  padding: 15px 20px;
+  padding: 15px 15px;
   background-color: teal;
   color: #fff;
   cursor: pointer;
+  margin-bottom: 10px;
 `;
-const Register = () => {
+const Link = styled.a`
+  margin: 5px 0px;
+  font-size: 12px;
+  text-decoration: underline;
+  cursor: pointer;
+`;
+
+const Login = () => {
   return (
     <Container>
       <Wrapper>
-        <Title>CREATE AN ACCOUNT</Title>
+        <Title>SIGN IN</Title>
         <Form>
-          <Input placeholder="First Name" />
-          <Input placeholder="Last Name" />
           <Input placeholder="Username" />
-          <Input placeholder="Email" />
           <Input placeholder="Password" />
-          <Input placeholder="Confirm Password" />
-          <Agreement>
-            By creating an account, I consent to the processing of my personal
-            data in accordance with the <b>PRIVACY POLICY</b>
-          </Agreement>
-          <Button>CREATE</Button>
+          <Button>Login</Button>
+          <Link>Forget Password?</Link>
+          <Link>Create New Account</Link>
         </Form>
       </Wrapper>
     </Container>
   );
 };
 
-export default Register;
+export default Login;
