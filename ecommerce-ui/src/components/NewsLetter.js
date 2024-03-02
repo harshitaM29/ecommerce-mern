317ec33280c6adf1d26475cb259d89224a0bf5a1
harshitaM29 <harshita.mundhe29@gmail.com>
2024-03-02 09:13:07 +0530

    newsletter component

---
+++ ecommerce-ui/src/components/NewsLetter.js
@@ -0,0 +1,58 @@
+import SendIcon from "@mui/icons-material/Send";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 60vh;
+  background-color: #fcf5f5;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Title = styled.h1`
+  font-size: 50px;
+  margin-bottom: 10px;
+`;
+const Description = styled.div`
+  font-size: 25px;
+  margin-bottom: 10px;
+`;
+const InputContainer = styled.div`
+  width: 50%;
+  height: 40px;
+  background-color: white;
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid lightgray;
+`;
+const Input = styled.input`
+  border: none;
+  flex: 8;
+  padding-left: 20px;
+`;
+const Button = styled.button`
+  flex: 1;
+  border: none;
+  background-color: teal;
+  color: white;
+`;
+
+const NewsLetter = () => {
+  return (
+    <Container>
+      <Title>NewsLetter</Title>
+      <Description>
+        Get timely updates from your favorite products.{" "}
+      </Description>
+      <InputContainer>
+        <Input placeholder="Enter your email" />
+        <Button>
+          <SendIcon />
+        </Button>
+      </InputContainer>
+    </Container>
+  );
+};
+
+export default NewsLetter;
--- ecommerce-ui/src/pages/Home.js
+++ ecommerce-ui/src/pages/Home.js
@@ -4,6 +4,7 @@ import Announment from "../components/Announment";
 import Slider from "../components/Slider";
 import Categories from "../components/Categories";
 import Products from "../components/Products";
+import NewsLetter from "../components/NewsLetter";
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
       <Slider />
       <Categories />
       <Products />
+      <NewsLetter />
     </div>
   );
 };
