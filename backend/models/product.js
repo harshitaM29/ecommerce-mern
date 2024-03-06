dd9b1689fd3d6571c436030494d244068ac4fcc5
harshitaM29 <harshita.mundhe29@gmail.com>
2024-03-06 09:55:34 +0530

    created model

---
+++ backend/models/product.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: Array,
+    },
+    size: {
+      type: String,
+    },
+    color: {
+      type: String,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Product", ProductSchema);
